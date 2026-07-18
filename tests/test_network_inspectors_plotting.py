9ae6963137b16dae8da6b98f31e9f8780d87158c
“mariama” <maria_ma@brown.edu>
2026-07-18 09:59:04 -0400

    Test interactive manifold surface values

--- tests/test_network_inspectors_plotting.py
+++ tests/test_network_inspectors_plotting.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 
@@ -26,4 +27,10 @@ def test_plot_manifold_returns_interactive_plotly_figure(tmp_path):
 
     assert isinstance(fig, go.Figure)
     assert fig.data[0].type == "surface"
+    np.testing.assert_array_equal(fig.data[0].x, [-2.0, -1.0, 1.0, 2.0])
+    np.testing.assert_array_equal(fig.data[0].y, [0.1, 0.2])
+    np.testing.assert_array_equal(
+        fig.data[0].z,
+        [[0.2, 0.1, 0.3, 0.4], [0.3, 0.2, 0.4, 0.5]],
+    )
     assert (tmp_path / "mlp_manifold_ddm.html").exists()
